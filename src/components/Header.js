abe48b32bd6e73e8f9d6d05814ac3a0424c5c65d
annie1004619 <annie1004619@naver.com>
2021-05-20 03:38:53 +0900

    fix: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ import {Link} from 'react-router-dom';
 function header() {
     return (
         <div className="header">
-            <Link to="/"><img src="/images/logo.png" alt="logo" /></Link>
+            <Link to="/"><img src="/images/logo.png" alt="logo" style={{marginLeft: '10px'}}/></Link>
             <div className="header_icon">
             <Link to="/mypage"><FontAwesomeIcon icon={faUser} size="2x" style={{marginLeft:'8px', color:"#40804F" ,width:'20px'}}/></Link>
              <Link to="/ranking"><FontAwesomeIcon icon={faMedal} size="2x"  style={{marginLeft:'8px', color:"#40804F",width:'25px' }}/></Link>
-            <Link to="/challenge/all"><FontAwesomeIcon icon={faCompass} size="2x"  style={{marginLeft:'8px', color:"#40804F",width:'23px'}}/></Link>
+            <Link to="/challenge/all"><FontAwesomeIcon icon={faCompass} size="2x"  style={{marginLeft:'8px', marginRight:'10px',color:"#40804F",width:'23px'}}/></Link>
             </div>
         </div>
     )
