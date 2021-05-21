e1f6f59f4480efebc3fffcf98434451072dbabee
RyuJungSik <76714485+RyuJungSik@users.noreply.github.com>
2021-05-21 11:24:52 +0900

    디자인 수정

--- src/components/Main/MainPage_Start.js
+++ src/components/Main/MainPage_Start.js
@@ -5,15 +5,15 @@ function MainPage_Start() {
     return (
         <div className="MainPage_Start">
             <div className="MainPage_Start_title">
-                <p>함께 실천해 가는 저탄소 챌린지</p>
+                <p>.</p>
             </div>
             <div className="MainPage_Start_desc">
-                <p><strong>챌린챌린</strong>은</p>
+                <p>챌린챌린은</p>
                 <p>우리 모두 참여할 수 있는</p>
                 <p>환경 챌린지 입니다.</p>
             </div>
             <div className="MainPage_Start_btn">
-                <p>생활 속 작은 챌린지로 세상을 바꿔볼까요?</p>
+                <p>생활 속 작은 챌린지로<br /> 세상을 바꿔볼까요?</p>
                 <Link to="/challenge/all">
                     <button>챌린지 바로 참여하기</button>
                 </Link>
