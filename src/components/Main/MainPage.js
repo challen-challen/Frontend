c6d16a980a47b24647deab930c9cc64657af8013
annie1004619 <annie1004619@naver.com>
2021-05-22 04:22:21 +0900

    fix: user reducer

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import MainPage_Challenge from './MainPage_Challenge'
 import MainPage_Charts from './MainPage_Charts'
 import MainPage_Current from './MainPage_Current'
 import MainPage_Info from './MainPage_Info'
 import MainPage_Start from './MainPage_Start'
+import axios from "axios";
 
 function MainPage() {
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/setUser', {withCredentials: true}).then(
+            (response) =>
+                localStorage.setItem('user', response.data.user._id)
+        ).then(error => error)
+    }, [])
+
     return (
         <div id="main_page">
-           <MainPage_Current />
-           <MainPage_Start />
-           <MainPage_Info />
-           <MainPage_Charts />
-           <MainPage_Challenge/>
+            <MainPage_Current/>
+            <MainPage_Start/>
+            <MainPage_Info/>
+            <MainPage_Charts/>
+            <MainPage_Challenge/>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import {store} from "./store/store";
 import "./components/Header.css";
 import "./components/Footer.css";
 import "./components/Main/MainPage.css";
 import "./components/Posting/Posting.css";
 import "./components/MyPage/MyPage.css";
-import axios from "axios";
+import {setUserTemp} from "./actions/user_actions";
 
-async function loadUser () {
-    await axios.get('http://localhost:5000/api/setUser').then(
-        response=>console.log(response)
-    ).then(error => error)
+function loadLocalUser() {
+    try {
+        const user = localStorage.getItem("user");
+        if (!user) return;
+        store.dispatch(setUserTemp())
+
+    } catch (e) {
+        console.log('localStorage is not working');
+    }
 }
 
-loadUser();
+loadLocalUser();
 
 ReactDOM.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
+    <Provider store = {store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
 
