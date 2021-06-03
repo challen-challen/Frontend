86f18414cee54c45994713a55352f6349d29b33f
annie1004619 <annie1004619@naver.com>
2021-06-03 19:42:41 +0900

    fix: mypage myfeed

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -4,6 +4,7 @@ import UserInfo from "./UserInfo";
 import {useSelector} from "react-redux";
 import Loading from "../Loading/Loading"
 import axios from "axios";
+import {MdKeyboardArrowRight,MdKeyboardArrowLeft} from 'react-icons/md'
 
 function MyPage() {
     const [myFeed, setMyFeed] = useState([])
@@ -11,13 +12,12 @@ function MyPage() {
     useEffect(() => {
         axios.get(`http://localhost:5000/api/mypage/posts`,  {withCredentials: true}).then(
             response =>{
-                
-                console.log(response.data.userPosts.post);
+                setMyFeed(response.data.userPosts.post)
             }
         ).then(error => error)
     }, [])
 
-
+console.log(myFeed)
     const user = useSelector(state => state.user.user)
     if (!user) {
         return <Loading/>
@@ -34,16 +34,23 @@ function MyPage() {
                 <div className="MyPage_UserFeed_title">
                     <p>나의 피드</p>
                 </div>
-                <div className="MyPage_UserFeed_desc">
-                    <img src="images/tumblr.png" alt="tumblr" />
-                    <img src="images/tumblr.png" alt="tumblr" />
-                    <br/>
-                    <img src="images/tumblr.png" alt="tumblr"/>
-                    <img src="images/tumblr.png" alt="tumblr"/>
-                    <div className="MyPage_UserFeed_btn">
-                        <p>내가 올린 피드를 보러 갈까요?</p>
-                        <button>나의 피드 보기</button>
-                    </div>
+                <div className="MyPage_UserFeed_container">
+                    {myFeed.length === 0 && <div style={{textAlign:'center'}}>loading...</div>}
+                <ul className="MyPage_UserFeed_desc">
+                    {myFeed && myFeed.slice(0,4).map(({fileUrl})=>
+                       <li><img src={`${fileUrl}`} alt={fileUrl} /></li>
+                    )}
+                </ul>
+                </div>
+                <div className="btn_container">
+                <div className="next_btn">
+                <MdKeyboardArrowLeft size={30}/>
+               Previous
+                </div>
+                <div className="next_btn">
+                    Next
+                <MdKeyboardArrowRight size={30}/>
+                </div>
                 </div>
             </div>
         </div>
