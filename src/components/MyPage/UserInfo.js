4ee171a3c3f9ce73a1008ee14892a55239bb5813
김지원 <69506987+annie1004619@users.noreply.github.com>
2021-06-04 22:27:36 +0900

    fix: userInfo

--- src/components/MyPage/UserInfo.js
+++ src/components/MyPage/UserInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './UserInfo.css'
+import {MdAccountCircle} from 'react-icons/md'
 import {useHistory} from "react-router";
 import {useDispatch} from "react-redux";
 import {logout} from '../../actions/user_actions'
@@ -17,8 +18,10 @@ function UserInfo({user}) {
         <div className="userInfo">
             <p>유저 정보</p>
             <ul className="infoContainer">
-                <li>{user && user.nickname}</li>
-                <li>{user?.post && user.post.length}개</li>
+                <li id="user"><MdAccountCircle size={30} style={{marginRight:'1vw'}}/>{user && user.nickname}</li>
+                <li>총 탄소 감량량: <strong  style={{backgroundColor: 'rgba(64,124,79,0.2)'}}>{user?.reducedCarbon?.allAmount}+ mg</strong></li>
+                <li>오늘의 감량량: <strong  style={{backgroundColor: 'rgba(64,124,79,0.2)'}}>{user?.reducedCarbon?.dailyAmount}</strong></li>
+                <li>참여한 챌린지 수:<strong  style={{backgroundColor: 'rgba(64,124,79,0.2)'}}>{user?.post && user.post.length}개</strong></li>
                 <input type="button" value="로그아웃" onClick={handleLogout}/>
             </ul>
         </div>
